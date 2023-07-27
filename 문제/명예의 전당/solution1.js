f4555fba75b8d617c0652cd7020ee052b8462d87
junyoungNA <nanasooo@naver.com>
2023-07-27 22:24:02 +0900

    명예의 전당 풀기

--- 문제/명예의 전당/solution1.js
+++ 문제/명예의 전당/solution1.js
@@ -1,38 +1,21 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/138477
 
-// function solution(k, score) {
-//     const answer = [score[0]];
-//     let min = score[0];
-    
-//     for(let i = 1; i <= 7 ; i++) {
-//         for(let j = i < k ? i + 1 : ; j < answer.length; j++) {
-//             if(score[j] < min) min = score[j];
-//         }
-//         answer.push(min);
-//     }
-//     console.log(answer, 'answer')
-//     return answer;
-// }
-
 function solution(k, score) {
     const answer = [score[0]];
     let fooArr = [score[0]]
     for(let i = 1; i < score.length; i++) {
-        // const limit = i < k ? i + 1 : score.length;
-        if(i <= k) {
-            console.log(score[i],'score[i]');
+        if(i < k) {
             fooArr.push(score[i]);
         }else {
             fooArr.push(score[i]);
             fooArr.sort((a, b) => a - b);
-            if(fooArr[0] > fooArr[1]) fooArr.splice(1,1)
-            else fooArr.shift();
+            if(fooArr[0] > fooArr[1]) fooArr.splice(1,1);
+            fooArr.shift();
         }
+        answer.push(Math.min(...fooArr));
     }
-    console.log(fooArr, 'fooArr');
-    console.log(answer, 'answer');
     return answer;
 }
 
 console.log(solution(3,[10, 100, 20, 150, 1, 100, 200])); //[10, 10, 10, 20, 20, 100, 100]
-// console.log(solution(4,[0, 300, 40, 300, 20, 70, 150, 50, 500, 1000])); //[0, 0, 0, 0, 20, 40, 70, 70, 150, 300]
+console.log(solution(4,[0, 300, 40, 300, 20, 70, 150, 50, 500, 1000])); //[0, 0, 0, 0, 20, 40, 70, 70, 150, 300]
