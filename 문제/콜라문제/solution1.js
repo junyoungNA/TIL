690ab252f7076227cc2b2bba97ee0012fd48fc0c
junyoungNA <nanasooo@naver.com>
2023-10-09 23:52:12 +0900

    테스트 콜라문제 2개빼고 오류

--- 문제/콜라문제/solution1.js
+++ 문제/콜라문제/solution1.js
@@ -3,15 +3,12 @@ function solution(a, b, n) {
     let answer = 0;
         
     while(n >= a)  {
-        const receive= Math.floor(n / a);
+        const receive= Math.floor(n / a) * b;
         answer += receive;
-        console.log(Math.floor(n / a), '받은');
-        console.log(n,'콜라');
-        console.log(n - receive,'남은');
-        n = n - receive;
+        n = n - (receive * a) + receive;
     }
     return answer;
 }
 
 console.log(solution(2, 1, 20)) //19
-console.log(solution(3, 1, 9)) //9
+console.log(solution(3, 1, 20)) //9
