ffe432b8d0239b64ebc910885c9151bcded7116c
junyoungNA <115803423+junyoungNA@users.noreply.github.com>
2023-11-08 23:17:31 +0900

    옹알이2 문제 다른사람코드로 풀어보기

--- 문제/옹알이2/solution4.js
+++ 문제/옹알이2/solution4.js
@@ -1,28 +1,45 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/133499?language=javascript
-
 function solution(babbling) {
-    //가져온코드
-    var answer = 0;
+    const words = ["aya", "ye", "woo", "ma"]
+    let answer = 0;
 
-    babbling=babbling.map((item)=>{
-        let tmp = item.replaceAll('aya','').replaceAll('ye','').replaceAll('woo','').replaceAll('ma','');
+    // 주어진 단어를 하나씩 테스트 해봅니다.
+    babbling.map(babyword => {
+        let progress = true; //while 문을 종료할 값
+        let before = "";
 
-        return tmp=='' ? item.replaceAll('aya','1').replaceAll('ye','2').replaceAll('woo','3').replaceAll('ma','4') : item;
-    })    
-
-
-    babbling.filter((item)=>item<'a').map((item)=>{
-        let flag=true;
-
-        let tmp = item.split('');
-
-        for (let i=0; i<tmp.length-1; i++){
-
-            flag = tmp[i] == tmp[i+1] ? false : flag;
+        while (progress) {
+            for (let i = 0; i < words.length; i++) {
+                // 단어의 맨 앞이 머쓱이가 읽을 수 있는 부분인가?
+                if (babyword.indexOf(words[i]) === 0) {
+                    console.log(babyword.indexOf(words[i]), words[i],'뭐가나오지?');
+                    if (before !== words[i]) {
+                         // 읽을 수 있더라도 연속하지는 않는가?
+                        before = words[i]; //검사한 단어를 중복체크하기하기위해 before에 대입
+                        babyword = babyword.slice(words[i].length);
+                        console.log('짤린', babyword);
+                        break;
+                    } else {
+                        progress = false;
+                        break;
+                    }
+                } else {
+                    console.log(words[i], babyword,'현재단어');
+                    if (i == words.length - 1) {
+                        progress = false;
+                        break;
+                    }
+                }
+            }
+            // 한 차례 판별이 끝난 후, 끝까지 다 읽었다면 발음 가능한 단어의 수(answer) + 1
+            if (babyword.length === 0) {
+                answer++;
+                progress = false;
+            }
         }
-
-        answer += flag ? 1 : 0
     })
-
     return answer;
 }
+
+console.log(solution(["aya", "yee", "u", "maa"]))//1
+// console.log(solution(["ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"]	))//2
---
+++ 문제/옹알이2/solution5.js
@@ -0,0 +1,18 @@
+//가져온코드
+function solution(babbling) {
+    const babblables = ["aya", "ye", "woo", "ma"];
+
+    return babbling.reduce((possible, babbl, index) => {
+        for (let i = 0; i < babblables.length; i += 1) {
+            if (babbl.includes(babblables[i].repeat(2))) return possible;
+        }
+
+        for (let i = 0; i < babblables.length; i += 1) {
+            babbl = babbl.split(babblables[i]).join(' ').trim();
+        }
+
+        if (babbl) return possible;
+
+        return possible += 1;
+    }, 0)
+}
