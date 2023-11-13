bbd3ce58069ed851682b8d5bd676a2a62894a338
junyoungNA <115803423+junyoungNA@users.noreply.github.com>
2023-11-13 11:56:46 +0900

    체유복 solution1 13,14 케이스 실패

--- 문제/체육복/solution1.js
+++ 문제/체육복/solution1.js
@@ -6,22 +6,31 @@
 function solution(n, lost, reserve) {
     let answer = n;
     let failNum = 0;
-    for(let i = 0; i < lost.length; i++) {
-        if(lost[i] > n || lost[i] < 1) continue;
-        if(lost[i] > 1)  {
-            const findIdx = reserve.findIndex((item) => item === lost[i] - 1);
-            // console.log('들어옴', i, lost[i], findIdx);
-            if(findIdx !== -1) continue;
-            failNum += 1;
-            
+
+    const newLost = lost.filter((item) => (!reserve.includes(item)));
+    const newReserve = reserve.filter((item) => (!lost.includes(item)));
+    console.log('어케됨?', newLost);
+    if(newLost.length < 1 ) return answer - failNum;
+
+    for(let i = 0; i < newLost.length; i++) {
+        if(newLost[i] > n || newLost[i] < 1 ) continue;
+        if(newLost[i] > 1)  {
+            const findIdx = newReserve.findIndex((item) => item === newLost[i] - 1);
+            console.log('들어옴', i, newLost[i], findIdx);
+            if(findIdx !== -1){
+                newReserve.splice(findIdx, 1);
+                continue;
+            } 
         }; 
-        if(lost[i] < n){
-            
-            const findIdx = reserve.findIndex((item) => item === lost[i] + 1);
-            // console.log('2들어옴', i, lost[i], findIdx);
-            if(findIdx !== -1) continue;
-            failNum += 1;
+        if(newLost[i] < n){
+            const findIdx = newReserve.findIndex((item) => item === newLost[i] + 1);
+            console.log('2들어옴', i, newLost[i], findIdx);
+            if(findIdx !== -1) {
+                newReserve.splice(findIdx, 1);
+                continue;
+            }
         } 
+        failNum += 1;
     }
     console.log(failNum,'봅시다');
     return answer - failNum;
@@ -29,6 +38,11 @@ function solution(n, lost, reserve) {
 
 
 // console.log(solution(5,[2, 4],[1, 3, 5])) //5
-console.log(solution(5,[2, 4],	[3]	)) //4
+// console.log(solution(5,[2, 4],	[3]	)) //4
+// console.log(solution(5, [3, 4], [1, 2, 5])) //5
+// console.log(solution(5, [1, 2], [3, 4])) //4
+// console.log(solution( 5, [4, 5], [3, 4]))//4
+console.log(solution( 10, [1, 2, 3, 4, 5, 6], [1, 2, 3]))//7
+console.log(solution( 5,[1, 2, 3],[2, 3, 4])) //4
 // console.log(solution(3, [3],[1])) //2
 
