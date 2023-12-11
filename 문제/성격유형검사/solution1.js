0119c9d0c65cb6535b9fb3e27c76a7e621dbac14
junyoungNA <115803423+junyoungNA@users.noreply.github.com>
2023-12-12 02:38:03 +0900

    성격유형검사 테스트실패

--- 문제/성격유형검사/solution1.js
+++ 문제/성격유형검사/solution1.js
@@ -16,12 +16,46 @@
 // 매우 동의	어피치형 3점
 
 function solution(survey, choices) {
-    // const indicators = ['RT', 'CF', 'JM', 'AN'];
+    const indicators = ['RT', 'CF', 'JM', 'AN']; //순수 지표
+    const choiceSurveyPoint = [];
+    const choiceSurvey = [];
+    const answer = [0, 0, 0, 0];
     
     for(let i = 0; i < choices.length; i++) {
+        // console.log(i,choices[i], '번째');
         
+        if(choices[i] > 4) {
+            choiceSurvey.push(survey[i][1]);
+            // console.log('choices',4 - choices[i],'큼')
+            choiceSurveyPoint.push(choices[i]);
+            continue;
+        }
+        if(choices[i] < 4) {
+            choiceSurvey.push(survey[i][0]);
+            // console.log('choices',4 - choices[i],'작음')
+            choiceSurveyPoint.push(choices[i]);
+        }
     }
-    return answer;
+    console.log(choiceSurveyPoint,'point');
+    console.log(choiceSurvey,'choice');
+
+    for(let i = 0; i < choiceSurvey.length; i++) {
+        const findIDX = indicators.findIndex((item)=> item.includes(choiceSurvey[i]));
+        console.log(findIDX,'찾음');
+        if(findIDX < 0 || findIDX > indicators.length -1) continue;
+        console.log(answer, choiceSurveyPoint[i], indicators[findIDX],'번째');
+        if(answer[findIDX] === 0) {
+            answer[findIDX] += choiceSurveyPoint[i];
+        } else {
+            choiceSurvey[i]
+        }
+    }
+
+    for(let i = 0; i < answer.length; i++) {
+        //3보다 크면 긍정 -마이너스가 아니라면 부정 +가드렁감
+        answer[i] = answer[i] < 0 ? indicators[i][1] : indicators[i][0];
+    }
+    return answer.join('');
 }
 // 1 네오형 N,  1점
 // 2  콘형  C, 1점
@@ -29,4 +63,4 @@ function solution(survey, choices) {
 // 4  튜브형 T, 3점
 // 5 어치피형 A 1점
 console.log(solution(["AN", "CF", "MJ", "RT", "NA"],[5, 3, 2, 7, 5])) //TCMA
-console.log(solution(["TR", "RT", "TR"],[7, 1, 3])) //RCJA
+// console.log(solution(["TR", "RT", "TR"],[7, 1, 3])) //RCJA
