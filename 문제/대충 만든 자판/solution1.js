bd59039b6b83ef121242ee319f0e71a2b457d7d6
junyoungNA <115803423+junyoungNA@users.noreply.github.com>
2023-12-13 23:44:04 +0900

    대충 만든 자판 문제

---
+++ 문제/대충 만든 자판/solution1.js
@@ -0,0 +1,12 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/160586
+
+function solution(keymap, targets) {
+    var answer = [];
+    return answer;
+}
+
+console.log(solution(["ABACD", "BCEFD"],["ABCD","AABB"])) //[9, 4]
+console.log(solution(["AA"],["B"])) //	[-1]
+console.log(solution(["AGZ", "BSSS"],["ASA","BGZ"])) //	[4, 6]
+
+
