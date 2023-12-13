6c256798107fb926fe58ffb7abd67fe515be7dd1
junyoungNA <115803423+junyoungNA@users.noreply.github.com>
2023-12-14 01:42:06 +0900

    대충 만든 자판 문제 푸는 중

--- 문제/대충 만든 자판/solution1.js
+++ 문제/대충 만든 자판/solution1.js
@@ -1,12 +1,26 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/160586
 
 function solution(keymap, targets) {
-    var answer = [];
+    const answer = new Array(targets).fill(0);
+    for(let i = 0; i < targets.length; i++) {
+        const splitTarget = targets[i].split('');
+        console.log(splitTarget,'split');
+        for(let j = 0; j < splitTarget.length; j++) {
+            const findIDX = keymap[i].indexOf(splitTarget[j]);
+            console.log(findIDX, '찾음');
+            if(findIDX === -1) {
+                answer[i] = findIDX;    
+                continue;
+            }
+            answer[i] += findIDX + 1;
+            console.log(answer);
+        }
+    }
     return answer;
 }
 
-console.log(solution(["ABACD", "BCEFD"],["ABCD","AABB"])) //[9, 4]
-console.log(solution(["AA"],["B"])) //	[-1]
-console.log(solution(["AGZ", "BSSS"],["ASA","BGZ"])) //	[4, 6]
+console.log(solution(["ABACD"],["ABCD"])) //[9, 4]
+// console.log(solution(["AA"],["B"])) //	[-1]
+// console.log(solution(["AGZ", "BSSS"],["ASA","BGZ"])) //	[4, 6]
 
 
