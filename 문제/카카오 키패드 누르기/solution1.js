b1e12826da7dc1bf3acff858c4daea253bb97e70
junyoungNA <nanasooo@naver.com>
2024-01-13 13:06:49 +0900

    카카오 키패드 문제 푸는 중

--- 문제/카카오 키패드 누르기/solution1.js
+++ 문제/카카오 키패드 누르기/solution1.js
@@ -4,6 +4,7 @@ function solution(numbers, hand) {
     let answer = '';
     let left = '*';
     let right = '#'
+    let middle = 0;
     numbers.map((item) => {
         console.log(item,'현재 숫자');
         switch(item) {
@@ -11,19 +12,30 @@ function solution(numbers, hand) {
             case 4: 
             case 7: 
                 answer += 'L';
+                left  = item;
                 break;
             case 2:
             case 5:
             case 8:
             case 0:
+                const calculLeft = item - left;
+                const calculRight = item - right;
+                const midValue = calculLeft > calculRight ? 'R' : 'L';
+                answer += midValue === 'R' ? 'R' : 'L';
+                if(midValue === 'R') right = item;
+                if(midValue === 'L') left = item;
+                console.log('왼쪽',left, '결과',calculLeft);
+                console.log('오른쪽', right, '결과', calculRight);
+                console.log('결과', answer);
+                
                 // 현재 오른손과 왼손의 위치중 가까운 손이 누르게됨
                 // 거리가 같다면 주손으로 뉼러야함
-                if()
                 break;
             case  3: 
             case  6: 
             case  9: 
                 answer += 'R';
+                right  = item;
                 break; 
             default : break;
         }
